005f8b6d374ce60d79532943269d75ac64c87ef0
alan <certain603@gmail.com>
2017-08-24 17:01:20 +0800

    use Vips compress the image

--- transform.go
+++ transform.go
@@ -20,7 +20,8 @@ import (
 	_ "image/gif" // register gif format
 	"image/jpeg"
 	"image/png"
-	
+
+	"github.com/daddye/vips"
 	"github.com/disintegration/imaging"
 	"willnorris.com/go/gifresize"
 )
@@ -68,15 +69,31 @@ func Transform(img []byte, opt Options) ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
+		return vips_transform(buf.Bytes() ,vips.JPEG )
 	case "png":
 		m = transformImage(m, opt)
 		err = png.Encode(buf, m)
 		if err != nil {
 			return nil, err
 		}
+		return vips_transform(buf.Bytes() , vips.PNG)
 	}
-
+	
 	return buf.Bytes(), nil
+}
+
+func vips_transform(img []byte , typ vips.ImageType) ([]byte, error) {
+	new_options := vips.Options{
+		Extend: vips.EXTEND_WHITE,
+		Interpolator: vips.NOHALO,
+		Format: typ,
+		Quality: 95,
+	}
+	new_buf, err := vips.Resize(img, new_options)
+	if err != nil {
+		return nil, err
+	}
+	return new_buf, nil
 }
 
 // resizeParams determines if the image needs to be resized, and if so, the
